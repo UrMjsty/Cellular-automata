b8b4749e0e02e306f8bb3970a521bfa3d27d6ed2
Cravcenco Dmitrii <dima16kravchenko@gmail.com>
2024-03-19 13:40:55 +0200

    Add zombie cell + optimize neighbours calculation

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -4,25 +4,20 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-
-public class Cell : MonoBehaviour
+public class Cell : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
 {
-    public SpriteRenderer spriteRenderer; //an image for showing state of cell
-    public bool isCellAlive; //store status of cell
-    private bool markedAlive, markedDead; //we will update status of cell in two steps.
-//first mark all cells dead or alive acording to it's neighbours
-//then update state of cells after all cells are marked
-    public Color aliveColor, deadColor; //colours we set from editor to indicate dead and alive cell
+    public SpriteRenderer spriteRenderer;
+    public bool isCellAlive;
+    private bool markedAlive, markedDead, markedZombie;
+    public Color aliveColor, deadColor, zombieColor; // Add color for Zombie cells
 
-//set colour of cell at the start according to starting state
     private void Awake() {
-        //image = GetComponent<Image>();
         aliveColor.a = 1;
         deadColor.a = 1;
-       spriteRenderer.color = isCellAlive ? aliveColor : deadColor;
+        zombieColor.a = 1; // Initialize alpha for Zombie color
+        spriteRenderer.color = GetCellColor();
     }
 
-//mark a cell as dead or alive. This will not change the state, just mark for later
     public void MarkDead() {
         markedDead = true;
     }
@@ -31,7 +26,10 @@ public class Cell : MonoBehaviour
         markedAlive = true;
     }
 
-//update the state of cell. The cell will be dead or alive if it was marked previously
+    public void MarkZombie() {
+        markedZombie = true;
+    }
+
     public void UpdateCell() {
         if (markedAlive) {
             ActivateCell();
@@ -39,26 +37,39 @@ public class Cell : MonoBehaviour
         if (markedDead) {
             DeactivateCell();
         }
+        // Reset Zombie mark after updating cell
+        markedZombie = false;
     }
 
-//theese methods will kill and revive cells
     public void ActivateCell() {
         markedAlive = false;
         markedDead = false;
         isCellAlive = true;
 
-        spriteRenderer.color = aliveColor; //update graphics
+        spriteRenderer.color = GetCellColor();
     }
 
     public void DeactivateCell() {
         markedAlive = false;
         markedDead = false;
         isCellAlive = false;
-        
-        spriteRenderer.color = deadColor; //update graphics
+
+        spriteRenderer.color = GetCellColor();
     }
 
-//mouse pointers input handlere. cell will switch state if clicked
+    private Color GetCellColor() {
+        if (isCellAlive) {
+            return aliveColor;
+        } else {
+            // Check if it's a Zombie cell
+            if (markedZombie) {
+                return zombieColor;
+            } else {
+                return deadColor;
+            }
+        }
+    }
+
     public void OnPointerEnter(PointerEventData eventData) {
         if (Input.GetMouseButton(0)) {
             if (isCellAlive) {
--- Assets/Scripts/GOLController.cs
+++ Assets/Scripts/GOLController.cs
@@ -6,126 +6,133 @@ using Random = System.Random;
 
 public class GOLController : MonoBehaviour
 {
-    public float updateTime; //call update cells to update cell. We can use coroutines or just create timer system to make repreated calls to it
+    public float
+        updateTime; //call update cells to update cell. We can use coroutines or just create timer system to make repreated calls to it
+
     public int cellCount_X, cellCount_Y; //number of cells in row and column
     public Cell[][] cells; //holder to store cells. we will store them in row and columt
     public GameObject cellPrefab; //cell prefab that will be initialized
     public Transform parent; //parent to hold all cells in game
     public int time = 0;
     public int frames = 100;
+
     void Initiate()
     {
 //      cells[0][0] = gameObject.AddComponent<Cell>();
-      /*cells[0][1] = gameObject.AddComponent<Cell>();
-      cells[1][0] = gameObject.AddComponent<Cell>();
-      cells[1][1] = gameObject.AddComponent<Cell>();*/
+        /*cells[0][1] = gameObject.AddComponent<Cell>();
+        cells[1][0] = gameObject.AddComponent<Cell>();
+        cells[1][1] = gameObject.AddComponent<Cell>();*/
     }
-    void CreateCells() {
-      cells = new Cell[cellCount_X][];
-      for (int i = 0; i < cellCount_X; i++) {
-        cells[i] = new Cell[cellCount_Y];
-        for (int j = 0; j < cellCount_Y; j++) {
-          //instantiate cells and store them
-          var rand = new Random().Next(0, 2);
-            Cell cell = Instantiate(cellPrefab, new Vector3(i - 10, j - 5), Quaternion.identity, parent).GetComponent<Cell>();
-            if(rand > 0)
-            {
-              cell.isCellAlive = true;
-            }
-            cells[i][j] = cell;
 
-          //  cells[i][j].name = $ "cell{i}{j}";
+    void CreateCells()
+    {
+        cells = new Cell[cellCount_X][];
+        for (int i = 0; i < cellCount_X; i++)
+        {
+            cells[i] = new Cell[cellCount_Y];
+            for (int j = 0; j < cellCount_Y; j++)
+            {
+                // Instantiate cells and store them
+                var rand = new Random().Next(0,
+                    3); // Random number to determine cell type: 0 for dead, 1 for alive, 2 for zombie
+                Cell cell = Instantiate(cellPrefab, new Vector3(i - 10, j - 5), Quaternion.identity, parent)
+                    .GetComponent<Cell>();
+                switch (rand)
+                {
+                    case 0: // Dead cell
+                        cell.isCellAlive = false;
+                        break;
+                    case 1: // Alive cell
+                        cell.isCellAlive = true;
+                        break;
+                    case 2: // Zombie cell
+                        cell.isCellAlive = true; // Zombie cells are initially alive
+                        cell.MarkZombie(); // Mark the cell as a zombie
+                        break;
+                }
+
+                cells[i][j] = cell;
+            }
         }
-      }
     }
-  void UpdateCells() {
-    //start checking and marking cells
-    for (int i = 1; i < cells.Length - 1; i++) {
-      for (int j = 1; j < cells[i].Length - 1; j++) {
-        //find numbers of live neighbours. Also don't forget to skip corner and edge cells. they will have fewer number of neighbours.
+
+    void UpdateCells()
+    {
+        //start checking and marking cells
+        for (int i = 1; i < cells.Length - 1; i++)
+        {
+            for (int j = 1; j < cells[i].Length - 1; j++)
+            {
+                int liveNeighbours = CountLiveNeighbors(i, j);
+                
+                // Rule 4: Mark cells as zombie if they have exactly 4 live neighbors
+                if (!cells[i][j].isCellAlive && liveNeighbours >= 4) {
+                    cells[i][j].MarkZombie();
+                }
+
+                //now after finding the neighbour, we can check rule to mark them dead of alive for next update
+                //Rule 1: A live cell with 2 or 3 alive neighbouring cells survives
+                if (cells[i][j].isCellAlive && (liveNeighbours == 2 || liveNeighbours == 3))
+                {
+                    continue;
+                }
+
+                //Rule 2:A dead cell with 3 neighbouring cells will get alive
+                if (!cells[i][j].isCellAlive && liveNeighbours == 3)
+                {
+                    cells[i][j].MarkAlive();
+                    continue;
+                }
+
+                //Rule 3: All other cells dies
+                cells[i][j].MarkDead();
+            }
+        }
+
+        //All cells are marked so update all cells.
+        for (int i = 0; i < cells.Length; i++)
+        {
+            for (int j = 0; j < cells[i].Length; j++)
+            {
+                cells[i][j].UpdateCell();
+            }
+        }
+    }
+    
+    int CountLiveNeighbors(int x, int y) {
         int liveNeighbours = 0;
 
-        //check bottom, left
-        if (i > 0 && j > 0 && cells[i - 1][j - 1].isCellAlive) {
-          liveNeighbours++;
+        // Check neighboring cells
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                if (dx == 0 && dy == 0) continue; // Skip the current cell
+                int nx = x + dx;
+                int ny = y + dy;
+                if (nx >= 0 && nx < cells.Length && ny >= 0 && ny < cells[x].Length && cells[nx][ny].isCellAlive) {
+                    liveNeighbours++;
+                }
+            }
         }
 
-        //check bottom
-        if (j > 0 && cells[i][j - 1].isCellAlive) {
-          liveNeighbours++;
-        }
-
-        //check bottom right
-        if (i < cells.Length - 1 & j > 0 && cells[i + 1][j - 1].isCellAlive) {
-          liveNeighbours++;
-        }
-
-        //check for right
-        if (i > 0 && cells[i - 1][j].isCellAlive) {
-          liveNeighbours++;
-        }
-
-        //check for left
-        if (i < cells.Length - 1 && cells[i + 1][j].isCellAlive) {
-          liveNeighbours++;
-        }
-
-        //check for top left
-        if (i > 0 && j < cells[i].Length - 1 && cells[i - 1][j + 1].isCellAlive) {
-          liveNeighbours++;
-        }
-
-        //check for top
-        if (j < cells[i].Length - 1 && cells[i][j + 1].isCellAlive) {
-          liveNeighbours++;
-        }
-
-        //check for top right
-      if (i < cells.Length - 1 && j < cells[i].Length - 1 && cells[i + 1][j + 1].isCellAlive) {
-        liveNeighbours++;
-      }
-
-      //now after finding the neighbour, we can check rule to mark them dead of alive for next update
-      //Rule 1: A live cell with 2 or 3 alive neighbouring cells survives
-      if (cells[i][j].isCellAlive && (liveNeighbours == 2 || liveNeighbours == 3)) {
-        continue;
-      }
-      //Rule 2:A dead cell with 3 neighbouring cells will get alive
-      if (!cells[i][j].isCellAlive && liveNeighbours == 3) {
-        cells[i][j].MarkAlive();
-        continue;
-      }
-
-      //Rule 3: All other cells dies
-      cells[i][j].MarkDead();
-      }
+        return liveNeighbours;
     }
 
-    //All cells are marked so update all cells.
-      for (int i = 0; i < cells.Length; i++) {
-        for (int j = 0; j < cells[i].Length; j++) {
-          cells[i][j].UpdateCell();
-        }
-      } 
-
-}
-
-  private void Awake()
-  {
-    CreateCells();
-    Initiate();
-  }
-
-  private void Update()
-  {
-    time++;
-
-    if (time % frames == 0)
+    private void Awake()
     {
-      UpdateCells();
+        CreateCells();
+        Initiate();
     }
 
-    if (time > 100)
-      time = 0;
-  }
+    private void Update()
+    {
+        time++;
+
+        if (time % frames == 0)
+        {
+            UpdateCells();
+        }
+
+        if (time > 100)
+            time = 0;
+    }
 }
